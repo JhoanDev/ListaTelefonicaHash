4bc318bcb3ebae292c6025c85f39025bca4cdb74
TIHeitorDS <heitorclaudinods@gmail.com>
2023-09-16 19:52:27 -0300

    feat: declaração dos protótipos das funções

--- src/telefone.h
+++ src/telefone.h
@@ -7,13 +7,28 @@ typedef struct telefone Telefone;
 // função para preencher um contato
 Telefone preenchedados();
 
-// cadastrar um novo contato na agenda
-void cadastra(Telefone *agenda,Telefone contato);
-
 // verifica se ha colisão
 int colisao(Telefone *agenda, int indice);
 
 // compara emails
 int compara(Telefone *agenda, int atual, int buscado);
+
+// cadastrar um novo contato na agenda
+void cadastra(Telefone *agenda,Telefone contato);
+
+// listar contatos da lista telefonica
+void listarcontatos(Telefone *agenda);
+
+// buscar contato
+int buscarcontato(Telefone *agenda);
+
+// importar contatos
+int importarcontatos(Telefone *agenda, char *caminho);
+
+//exportar contatos
+void exportarcontatos(Telefone *agenda, char *caminho);
+
+// gerar contatos
+void geracontatos(char *caminho);
 
 #endif // TELEFONE_H
