0d911446784b5a85925bf944beb4a3b64281404b
JhoanDev <jhoandev.log@gmail.com>
2023-09-15 22:35:28 -0300

    feat: soma diferente

--- src/telefone.h
+++ src/telefone.h
@@ -1,6 +1,9 @@
-//TAD: telefone
+// TAD: telefone
 
 typedef struct telefone Telefone;
 
-//Transforma a string em binaria
-char *StringParaBinario(const char *texto); 
+// Transforma a string em binaria
+char *StringParaBinario(const char *texto);
+
+// faz a soma "dobrando" o primeiro termo
+char *SomaDiferente(char *dobra, char *naodobra);
